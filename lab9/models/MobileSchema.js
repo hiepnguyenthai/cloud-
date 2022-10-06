2eb5c34780e646667be238975251b98cd9d556f5
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-10-06 17:37:29 +0700

    ok

--- lab9/models/MobileSchema.js
+++ lab9/models/MobileSchema.js
@@ -7,6 +7,9 @@ var MobileSchema = schema({
   brand: String,
   image: String,
   price: Number,
+},
+{
+  versionKey: false
 });
 
 var MobileModel = model("Điện thoại", MobileSchema, "mobile");
--- lab9/routes/mobile.js
+++ lab9/routes/mobile.js
@@ -24,4 +24,42 @@ router.post("/add", (req, res) => {
   });
 });
 
+router.get("/detail/:id", (req, res) => {
+  //lấy giá trị id của document gửi từ url
+  var mobile_id = req.params.id;
+  //tìm kiếm document trong collection theo id
+  MobileModel.findById(mobile_id, (err, data) => {
+    if (!err) {
+      //render ra file detail chứa dữ liệu của document
+      res.render("mobile/detail", { mobile: data });
+    }
+  });
+});
+
+router.get("/delete/:id", (req, res) => {
+  MobileModel.findByIdAndDelete(req.params.id, (err) => {
+    if (!err) {
+      console.log("Delete mobile succeed !")
+      res.redirect("/mobile")
+    }
+  })
+})
+
+router.get("/edit/:id", (req, res) => {
+   MobileModel.findById(req.params.id, (err, data) => {
+     if (!err) {
+        res.render("mobile/edit", { mobile: data })
+     }
+   })
+})
+
+router.post("/edit/:id", (req, res) => {
+    MobileModel.findByIdAndUpdate(req.params.id, req.body, (err) => {
+      if (!err) {
+        console.log("Edit mobile succeed !")
+        res.redirect("/mobile")
+      }
+    })
+})
+
 module.exports = router;
