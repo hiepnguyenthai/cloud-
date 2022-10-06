14d443bbf200d15b847069f068e2917d1e24445b
hiepnguyenthai <HiepNTGCH200524@fpt.edu.vn>
2022-10-06 15:22:31 +0700

    ok

--- lab7/app.js
+++ lab7/app.js
@@ -1,31 +1,27 @@
 const express = require("express");
 const app = express();
-const app = require("path");
+const path = require("path");
 const port = process.env.PORT || 3000;
 
 //cấu hình views & view engine
-app.use("views", path.join(__dirname, "/views"));
-app.use("view engine", "hbs");
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "hbs");
 
 //kết nối database
 const mongoose = require("mongoose");
-const MobileModel = require("./module/MobileSchema");
-const url = "mongodb://localhost:27017/demo"
-mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+const MobileModel = require("./models/MobileSchema");
+const url = "mongodb://localhost:27017/demo";
+mongoose.connect(url, { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+  MobileModel.find((err, data) => {
     if (err) {
-        console.log("DB connect error: " + err)
+      console.log(err);
     } else {
-        //console.log("Connect to DB succeed !");
-        app.get("/", (req, res) => {
-            MobileModel.find((err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(data);
-            }
-        });
-    });
-  }
+      //res.send(data);
+      console.log(data);
+    }
+  });
 });
 
 //chạy server
